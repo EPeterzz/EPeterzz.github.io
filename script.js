ff2ab5ee8760314bce02243b421342cb3effdc5a
이민수 <163666757+EPeterzz@users.noreply.github.com>
2024-04-11 13:39:34 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var el = document.getElementById('HOME');
+var myfunc = function () {
+    alert('addEventListener');
+}
+el.addEventListener("click", myfunc);
+
+var el = document.getElementById('ABOUT');
+var myfunc = function () {
+    alert('addEventListener');
+}
+el.addEventListener("click", myfunc);
+
+var el = document.getElementById('CONTACT');
+var myfunc = function () {
+    alert('addEventListener');
+}
+el.addEventListener("click", myfunc);
+
+var el = document.getElementById('DESIGNS');
+var myfunc = function () {
+    alert('addEventListener');
+}
+el.addEventListener("click", myfunc);
+
